fffbb0113116e042b8305aca7ec6224a775c5915
Magdy Zamel <magdy.zamel@robustastudio.com>
2019-12-09 03:25:21 +0200

    Build the repository  base

---
+++ MSZNearBy/Core/Architecture/Repository/RepositoryProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoryProtocol.swift
+//  MSZNearBy
+//
+//  Created by MSZ on 12/9/19.
+//  Copyright © 2019 MSZ. All rights reserved.
+//
+
+import Foundation
+import Promises
+
+protocol RepositoryProtocol {
+    func changeMCacheToDirty()
+    func startFromLocal() -> Promise<Bool>
+    func startFromRemote() -> Promise<Bool>
+}
